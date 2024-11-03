49a9ebf96d9b84ff69e141519f1d4d97fe4d34c8
Asaf31214 <asaf.kanlipicak@outlook.com>
2024-11-03 04:03:54 +0300

    project initiation

---
+++ src/client/java/org/siverek/cannon/client/CannonClient.java
@@ -0,0 +1,10 @@
+package org.siverek.cannon.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CannonClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/siverek/cannon/Cannon.java
@@ -0,0 +1,10 @@
+package org.siverek.cannon;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cannon implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
